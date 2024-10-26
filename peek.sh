b2bd4f3300d3ffa7731593780d1d187a611e52a2
Xavi Vilchez Perez <xavi1999vp@gmail.com>
2024-10-26 10:54:42 +0200

    peek_v3

--- peek.sh
+++ peek.sh
@@ -1,7 +1,17 @@
 input_file=$1
 n_lines=$2
 
-head -n $n_lines $input_file
-echo ...
-tail -n $n_lines $input_file
+if [[ -z $n_lines ]]; then 
+	n_lines=3
+fi
 
+if [[ $(cat $input_file | wc -l) -le $(( 2*$n_lines )) ]]; then 	
+	
+	cat $input_file
+
+else 
+	echo WARNING:$input_file has more than $(( 2*$n_lines )) lines!
+	head -n $n_lines $input_file
+	echo ...
+	tail -n $n_lines $input_file
+fi
