59eda04972ae6a25cfb6739db201caac32541b7f
Xavi Vilchez Perez <xavi1999vp@gmail.com>
2024-10-26 11:15:45 +0200

    countlines_v2

--- countlines.sh
+++ countlines.sh
@@ -1,14 +1,15 @@
-input_file=$1
+for input_file in "$@"; do
 
-echo $input_file
+	echo $input_file
 
-if [[ $(cat $input_file | wc -l) -eq 0 ]]; then
-	echo The file has 0 lines.
+	if [[ $(cat $input_file | wc -l) -eq 0 ]]; then
+		echo The file has 0 lines.
 
-elif [[ $(cat $input_file | wc -l) -eq 1 ]]; then
-	echo The file has 1 line.
+	elif [[ $(cat $input_file | wc -l) -eq 1 ]]; then
+		echo The file has 1 line.
 
-else 
-	echo The file has more than 1 line.
+	else 
+		echo The file has more than 1 line.
 
-fi
+	fi
+done
