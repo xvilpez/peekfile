fdbac79e2b56c149dc65039766abb7ce4f9ff6de
Xavi Vilchez Perez <xavi1999vp@gmail.com>
2024-11-25 19:25:16 +0100

    fastascan_v3

--- fastascan.sh
+++ fastascan.sh
@@ -5,45 +5,64 @@ X=$1
 N=$2
 [ -z $N ] && N=0
 
-fastas=$(find $X -type f -name "*.fa" -or -name "*.fasta")
+#Ensuring that the directory exists
+if [[ -d $X ]]; then 
 
-#Ensuring that there are fasta files in the variable fastas
-if [[ -n $fastas ]]; then
+	fastas=$(find $X -type f -name "*.fa" -or -name "*.fasta")
+	echo "#####START OF THE REPORT#####"
+	#Ensuring that there are fasta files in the variable fastas
+	if [[ -n $fastas ]]; then
 
-	#Reporting number of files
-	echo There are $(find $X -type f -name "*.fa" -or -name "*.fasta" | wc -l) fasta files in $X folder and subfolders.
-
-	#Reporting number of unique fasta IDs
-	echo There are $(grep ">" $fastas | awk '{print $1}' | sort | uniq | wc -l) unique fasta IDs.
-
-
-	for i in $fastas; do
-		
-		if [[ -s $i ]]; then
+		echo '#####Reporting number of files#####'
+		echo There are $(find $X -type f -name "*.fa" -or -name "*.fasta" | wc -l) fasta files in $X folder and subfolders.
+		echo
 			
-			grep -v ">" $i | sed 's/[- \n]//g' | ( grep -q [RDEQHILKMFPSWYV]  && echo fasta.p.file==================$i) || echo fasta.n.file==================$i
-			echo
+		echo '#####Reporting number of unique fasta IDs#####'
+		echo There are $(grep "^>" $fastas | awk '{print $1}' | sort | uniq | wc -l) unique fasta IDs.
+		echo
 			
-			([ -h $i ] && echo $i is a symlink.) || echo $i is not a symlink.
-			echo
-			
-			echo There are $(grep -c ">" $i) sequences inside $i. 
-			echo The total sequence length of $i is $(grep -v ">" $i | sed 's/[- \n]//g' | awk '{total=total + length} END {print total}').
-			echo
-			
-			if [[ $N -gt 0 ]]; then
-			([ $(cat $i | wc -l) -le $((2*$N)) ] && cat $i) || head -n $N $i; echo ...; tail -n $N $i
-			echo
+		echo '#####Starting report on all fasta files found#####'
+		echo
+		for i in $fastas; do
+			#Ensure that the file has permissions and content
+			if [[ -r $i ]]; then
+				if [[ -s $i ]]; then
+				
+					#Label the header differentiating between protein and nucleotide fasta files
+					grep -v "^>" $i | sed 's/[- \n]//g' | ( grep -q [RDEQHILKMFPSWYV]  && echo "###fasta.protein.file==================$i###") || echo "###fasta.nucleotide.file==================$i###"
+					echo
+					
+					#Classify between symlinks or regular files
+					[ -h $i ] && echo $i is a symlink. && echo
+						
+					#Counting the amount of sequences and the total aminoacids/nucleotides of that file
+					([ $(grep -c "^>" $i) -gt 1 ] && echo There are $(grep -c "^>" $i) sequences inside $i.) || echo There is 1 sequence inside $i.
+					echo The total sequence length of $i is $(grep -v "^>" $i | sed 's/[- \n]//g' | awk '{total=total + length} END {print total}').
+					echo
+					
+					if [[ $N -gt 0 ]]; then #Skips step if the number of lines is set to 0 (default parameter).
+					echo "#Showing content of $i"
+					echo
+
+					([ $(cat $i | wc -l) -le $((2*$N)) ] && cat $i) || head -n $N $i; echo ...; tail -n $N $i
+					echo
+					fi
+		###WARNINGS section		
+				else echo "###fasta.file==================$i###"; echo WARNING:$i IS AN EMPTY FASTA FILE!!!
+					echo
+				fi
+				
+			else echo echo "###fasta.file==================$i###"; WARNING: $i HAS NO READ PERMISSIONS!!!
+				echo
 			fi
-		
-		else echo fasta.file==================$i; echo WARNING:$i IS AN EMPTY FASTA FILE!!!!!!!
-			echo
-		fi
-	done
+		done
 
-	else echo WARNING:THERE ARE NO FASTA FILES IN $X FOLDER AND SUBFOLDERS.
-
+	else echo There are no fasta files in $X and subfolders.
+	fi
+	
+	echo "#####END OF THE REPORT#####"
+	
+else echo WARNING: THE FOLDER DOES NOT EXIST!!!
 fi
-
 
 
